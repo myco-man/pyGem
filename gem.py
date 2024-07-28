7328954627d129ae0f803db7b4d32abd1e84665c
myco-man <myc0.m4n@gmail.com>
2024-07-28 15:51:32 -0700

    looped + deleted redundancies

--- gem.py
+++ gem.py
@@ -7,13 +7,12 @@ import os
 Features:
     Retreive APi key from bashrc
     Variable output through flag usage
-    Help command
+    Help command - flesh out
     v2 - Format response - tabbed in + ?
     v1 - Preconfigured verbosity flags? cyclic calls to the api
     v2 - Formatting options?
 """
 api_key=os.environ['gemini_api_key']
-#stets
 
 
 class gem:
@@ -27,7 +26,7 @@ class gem:
     
     def __init__(self, args) -> None:
         self.myArgs =  args
-       
+
     def gemCall(self,):
         url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key='+api_key
         postHeaders = {"Content-Type": "application/json"}
@@ -42,6 +41,12 @@ class gem:
         self.finishReason=tx["candidates"][0]["finishReason"]
         self.chatIndex=tx["candidates"][0]["index"]
     
+    def setPrompt(self, newPrompt):
+        if newPrompt == 'exit':
+            return
+        self.myArgs.prompt = newPrompt
+       
+
     def printArgs(self):
         for ar in vars(self.myArgs):
             print(ar, getattr(self.myArgs, ar))
@@ -51,43 +56,26 @@ class gem:
         print("\n- - -      - - -      - - -      - - -")
         print(self.responseText)
         print("- - -      - - -      - - -      - - -\n")
-        if self.myArgs.usageData or self.myArgs.all:
+        if self.myArgs.all:
             print("| Usage MetaData:      - - -      - - -      - - -\n")
             print(self.usageMetadata)
-        if self.myArgs.finishReason or self.myArgs.all:
+           
             print("| Finish Reason:      - - -      - - -      - - -\n")
             print(self.finishReason)
-        if self.myArgs.safetyRatings or self.myArgs.all:
+            
             print("| Safety Ratings:      - - -      - - -      - - -\n")
             print(self.safetyRatings)
-
-    def responseFormatter(self): #wip
-        t = self.responseText
-
-        print(t)
-        print("##################################################")
-        print(t.replace('\n',' '))
-
-def main():
-    #build parser arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-gem', '--gemini', action='store_true')
-    parser.add_argument('-sr', '--safetyRatings', action='store_true')
-    parser.add_argument('-ud', '--usageData', action='store_true')
-    parser.add_argument('-fr', '--finishReason', action='store_true')
-    parser.add_argument('-ci', '--chatIndex', action='store_true')
-    parser.add_argument('-a', '--all', action='store_true')
-    parser.add_argument('prompt')
-    arggs = parser.parse_args()
-
-    #Create Gemini client instance + initiate call w/ prompt
-    g = gem(arggs)
-    g.gemCall()
-
-    g.printResponse()
-    # g.responseFormatter()
-
-if __name__ == "__main__":
-    main()
+        else:
+            if self.myArgs.usageData:
+                print("| Usage MetaData:      - - -      - - -      - - -\n")
+                print(self.usageMetadata)
+            
+            if self.myArgs.finishReason:
+                print("| Finish Reason:      - - -      - - -      - - -\n")
+                print(self.finishReason)
+            
+            if self.myArgs.safetyRatings:
+                print("| Safety Ratings:      - - -      - - -      - - -\n")
+                print(self.safetyRatings)
 
 
--- main.py
+++ main.py
@@ -1,2 +1,30 @@
-#test
-#test2
+#!/usr/bin/env python
+
+import gem 
+import argparse
+ 
+helpMessage ="You have been helped!"
+
+def main():
+    #build parser arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-gem', '--gemini', action='store_true')
+    parser.add_argument('-sr', '--safetyRatings', action='store_true', help="lists the safety rating returned")
+    parser.add_argument('-ud', '--usageData', action='store_true')
+    parser.add_argument('-fr', '--finishReason', action='store_true')
+    parser.add_argument('-ci', '--chatIndex', action='store_true')
+    parser.add_argument('-a', '--all', action='store_true')
+    #parser.add_argument('--help', action='store_true')
+    # parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS, help=helpMessage)
+    parser.add_argument('prompt')
+    arggs = parser.parse_args()
+    i = ""
+    while i != 'exit':
+    #Create Gemini client instance + initiate call w/ prompt
+        g = gem.gem(arggs)
+        g.gemCall()
+        g.printResponse()
+        i = input().lower()
+        g.setPrompt(i)
+if __name__ == "__main__":
+    main()
